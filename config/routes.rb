5f7a15df882c0dce083124b4330ceda0b16b7af2
Audrey Gehring <audreygehring@gmail.com>
2017-02-17 15:41:23 -0500

    devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root "homes#index"
 
+  devise_for :users, except: [:index]
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    post '/users/edit' => 'devise/registrations#edit'
+  end
+
+  resources :users, only: [:show]
+
   resources :books
-  
 end
