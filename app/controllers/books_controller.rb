569f82d72c4604187c72fa73fcbc33b422f85406
Audrey Gehring <audreygehring@gmail.com>
2017-02-18 13:11:18 -0500

    book show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,12 +26,29 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.udate(book_params)
+      flash[:notice] = "Book updated successfully!"
+      redirect_to @dish
+    else
+      flash[:notice] = @book.errors.full_messages.to_sentence
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book deleted"
+      redirect_to books_path
+    else
+      flash[:notice] = "Sorry, you can't delete someone else's book!"
+      render :show
+    end
   end
 
   private
