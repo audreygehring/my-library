0ec2cd2d6f89bdcdf98a29605c4fb57076ab8b3c
Audrey Gehring <audreygehring@gmail.com>
2017-05-16 18:48:19 -0400

    add book_id to booklist table

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   root "homes#index"
 
   devise_for :users, except: [:index]
-
   resources :users, only: [:show]
-
   resources :books
-
   resources :authors
+  resources :booklists
 end
